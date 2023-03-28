6dee8e78200b11bdae9d47f6b5184582930c99db
g.asmaro <g.asmaro@surreyschools.ca>
2023-03-28 14:02:14 -0700

    .

---
+++ src/Main.java
@@ -0,0 +1,117 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter 1 for calculating the sum of a range that you choose\n");
+        System.out.println("Enter 2 for calculating the sum of a range chosen with a random numbers generated");
+        int choice = input.nextInt();
+        switch (choice) {
+            case 1 -> {
+                System.out.println("Enter the starting number you would like to calculate the sum of the range");
+                int sumCalculationStart = input.nextInt();
+                System.out.println("Enter the ending number you would like to calculate the sum of the range");
+                int sumCalculationEnd = input.nextInt();
+                System.out.println(getTotalSum(sumCalculationStart,sumCalculationEnd));
+            }
+            case 2 -> {
+                System.out.println("Enter min number to generate from");
+                int min = input.nextInt();
+
+                System.out.println("Enter max number to generate from");
+                int max = input.nextInt();
+
+                System.out.println("Enter the amount of numbers you would like to randomly generate");
+                int amount = input.nextInt();
+
+                System.out.println("Total Sum: " + randomNumSum(min,max,amount));
+            }
+            default -> { System.out.println("incorrect input"); }
+        }
+
+    }
+
+    public static int getTotalSum(int rangeStart, int rangeEnd) {
+        int totalSum = 0;
+        int start = rangeStart;
+        for(int i = rangeStart; i <= rangeEnd; i++) {
+               totalSum += start;
+               start++;
+        }
+        return totalSum;
+    }
+
+    public static int randomNumSum(int randRange1, int randRange2, int numsToGenerate) {
+        int totalSum = 0;
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        Random rand = new Random();
+
+
+        for(int i = 0; i < numsToGenerate; i++) {
+            arrayList.add(rand.nextInt(randRange2 - randRange1 + 1) + randRange1);
+        }
+
+        int size = arrayList.size();
+
+
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println(arrayList.get(i));
+            totalSum += arrayList.get(i);
+        }
+
+
+        int mostCommon = findMostCommonElement(arrayList);
+        System.out.println("Most common element: " + findMostCommonElement(arrayList));
+        System.out.println("Average number: " + findAverageDataProduced(size, totalSum));
+        System.out.println("Percentage common num appeared: " + findPercentageOfData(mostCommon, arrayList, size));
+        return totalSum;
+    }
+
+    // What is the most common value in the output data?
+    public static int findMostCommonElement(ArrayList<Integer> list) {
+        int mostCommonElement = list.get(0);
+        int maxCount = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            int currentElement = list.get(i);
+            int count = 0;
+
+            for (int j = 0; j < list.size(); j++) {
+                if (currentElement == list.get(j)) {
+                    count++;
+                }
+            }
+
+            if (count > maxCount) {
+                maxCount = count;
+                mostCommonElement = currentElement;
+            }
+        }
+        return mostCommonElement;
+    }
+
+    // What is the average of all data produced?
+    public static double findAverageDataProduced(int length, int total){
+        // Iterate through the list of data
+        double average = (double)total/length;
+        return average;
+    }
+
+    // What % of the time did the most common value appear?
+    public static double findPercentageOfData(int mostCommon, ArrayList<Integer> listToSearch, int length) {
+        int amountInList = 0;
+        for(int i = 0; i < listToSearch.size(); i++) {
+            if(listToSearch.get(i) == mostCommon) {
+                amountInList++;
+            }
+        }
+
+        double percentage = (double)amountInList/length*100;
+        return percentage;
+    }
+
+}
