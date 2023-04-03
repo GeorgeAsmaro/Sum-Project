3a25d5c1ec0238d06f0e44c063ded4cd57202145
George Asmaro <67618081+GeorgeAsmaro@users.noreply.github.com>
2023-04-03 09:51:46 -0700

    Update OptimalStopping.java

--- src/OptimalStopping.java
+++ src/OptimalStopping.java
@@ -1,13 +1,17 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class OptimalStopping {
 
     int maxAttempts;
     int rating;
 
+    static Random rand = new Random();
+    static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+
     }
 
 }
