2d1f3760e02ca844c02d37bfe936ee0c0218b6dc
George Asmaro <67618081+GeorgeAsmaro@users.noreply.github.com>
2023-04-03 09:27:55 -0700

    i dont know how to start this situation

---
+++ src/OptimalStopping.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class OptimalStopping {
+
+    int maxAttempts;
+    int rating;
+
+    public static void main(String[] args) {
+        
+    }
+
+}
